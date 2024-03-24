9403e38df7116ecac8d04c8bb089169e7550635c
Jason Chong <jason.chong.1@stonybrook.edu>
2024-03-24 04:23:35 -0400

    docs(dht): add docs for the file metadata struct

--- market_dht/src/file.rs
+++ market_dht/src/file.rs
@@ -4,12 +4,17 @@ use multihash_codetable::{Code, MultihashDigest};
 use serde::{Deserialize, Serialize};
 use std::net::Ipv4Addr;
 
+/// Creates a new CIDv0 from the given data that is to be inserted into the DHT as the key
+///
+/// # Errors
+/// Fails based on the [Cid::new_v0] function result provided by the [Cid] crate
 pub fn new_cidv0(data: &[u8]) -> Result<Cid> {
     let h = Code::Sha2_256.digest(data);
     let cid = Cid::new_v0(h);
     Ok(cid?)
 }
 
+/// Metadata for the file that is stored in the DHT
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
 pub struct FileMetadata {
     ip: Ipv4Addr,
@@ -18,7 +23,7 @@ pub struct FileMetadata {
 }
 
 impl FileMetadata {
-    pub(crate) fn new(ip: Ipv4Addr, port: u16, price_per_mb: u64) -> Self {
+    pub(crate) const fn new(ip: Ipv4Addr, port: u16, price_per_mb: u64) -> Self {
         Self {
             ip,
             port,
@@ -26,15 +31,18 @@ impl FileMetadata {
         }
     }
 
-    pub fn ip(&self) -> Ipv4Addr {
+    /// Returns the IP address of the peer that is storing the file
+    pub const fn ip(&self) -> Ipv4Addr {
         self.ip
     }
 
-    pub fn port(&self) -> u16 {
+    /// Returns the port of the peer that is storing the file
+    pub const fn port(&self) -> u16 {
         self.port
     }
 
-    pub fn price_per_mb(&self) -> u64 {
+    /// Returns the price per MB of the file
+    pub const fn price_per_mb(&self) -> u64 {
         self.price_per_mb
     }
 }
