0a130eddbbf1b5591b9875815d7c20084821e722
Jason Chong <jason.chong.1@stonybrook.edu>
2024-03-31 23:26:32 -0400

    docs(dht): comment for clippy for adding docs
    Just so i use the clippy lints when i uncomment it

--- market_dht/src/lib.rs
+++ market_dht/src/lib.rs
@@ -1,9 +1,10 @@
 #![warn(
     missing_debug_implementations,
     // missing_docs,
+    // clippy::missing_docs_in_private_items,
     // clippy::missing_errors_doc,
     // clippy::missing_panics_doc,
-    clippy::missing_const_for_fn,
+    clippy::missing_const_for_fn
 )]
 #![deny(unsafe_code, unreachable_pub)]
 
