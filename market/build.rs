041fef90aa9c10fc407513fa97449e4752c1fc8d
Daniel Kogan <daniel.kogan@stonybrook.edu>
2024-04-13 14:58:55 -0400

    modify only peernode build.rs

--- market/build.rs
+++ market/build.rs
@@ -1,9 +1,5 @@
+// use serde::{Deserialize, Serialize};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let proto = "proto/market.proto";
-
-    tonic_build::configure()
-        .type_attribute("User", "#[derive(serde::Deserialize, serde::Serialize)]")
-        .compile(&[proto], &["../"])?;
-
+    tonic_build::compile_protos("../proto/market.proto")?;
     Ok(())
 }
