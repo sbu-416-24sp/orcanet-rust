564f7ccca7bc5c13e91bfcfc59be16bed5fa156d
tamandrew <andrew.c.tam@stonybrook.edu>
2024-04-25 11:16:00 -0400

    Fix job history type

--- peernode/src/peer-node/producer/files.rs
+++ peernode/src/peer-node/producer/files.rs
@@ -16,6 +16,7 @@ use tokio::sync::RwLock;
 pub struct FileMap {
     files: RwLock<HashMap<String, PathBuf>>,
     prices: RwLock<HashMap<String, i64>>,
+    fileNames: RwLock<HashMap<String, String>>,
 }
 
 pub type AsyncFileMap = Arc<FileMap>;
@@ -34,6 +35,7 @@ impl FileMap {
         FileMap {
             files: RwLock::new(HashMap::new()),
             prices: RwLock::new(HashMap::new()),
+            fileNames: RwLock::new(HashMap::new()),
         }
     }
 
@@ -41,6 +43,7 @@ impl FileMap {
         FileMap {
             files: RwLock::new(files),
             prices: RwLock::new(prices),
+            fileNames: RwLock::new(HashMap::new()),
         }
     }
 
@@ -60,12 +63,15 @@ impl FileMap {
         // Get a write lock on the files map
         let mut files = self.files.write().await;
         let mut prices = self.prices.write().await;
+        let mut fileNames = self.fileNames.write().await;
 
         // Open the file
         let mut file = File::open(file_path)?;
         let hash = hash_file(&mut file)?;
         files.insert(hash.clone(), file_path.into());
         prices.insert(hash.clone(), price);
+        fileNames.insert(hash.clone(), file_path.into());
+        
         Ok(hash)
     }
 
--- peernode/src/peer-node/producer/jobs.rs
+++ peernode/src/peer-node/producer/jobs.rs
@@ -6,7 +6,7 @@ type AsyncJob = Arc<Mutex<Job>>;
 
 pub struct Jobs {
     jobs: RwLock<HashMap<String, AsyncJob>>,
-    history: RwLock<Vec<Job>>,
+    history: RwLock<HashMap<String, HistoryEntry>>,
 }
 
 #[derive(Clone)]
@@ -20,6 +20,12 @@ pub struct Job {
     peerId: String,
 }
 
+#[derive(Clone)]
+pub struct HistoryEntry {
+    fileName: String,
+    timeCompleted: u64, // unix time in seconds
+}
+
 impl Jobs {
     pub fn new() -> Self {
         Jobs {
@@ -27,44 +33,48 @@ impl Jobs {
         }
     }
 
-    pub async fn add_job(&self, peerId: String, job: Job) -> AsyncJob {
+    pub async fn add_job(&self, peerId: String, fileName: string, job: Job) -> String {
+        let jobId = rand::thread_rng().gen::<u64>().to_string();
+
         // Get a write lock on the jobs map
         let mut jobs = self.jobs.write().await;
 
-        // Add the consumer to the map
+        // Add the job to the map
         let async_job = Arc::new(Mutex::new(job));
-        jobs.insert(peerId, async_job.clone());
+        jobs.insert(jobId, async_job.clone());
 
-        // Get a write lock on the job history
+        // Get a write lock on the job history map
         let mut history = self.history.write().await;
 
-        // Add the job to the history
-        history.push(async_job.lock().await.clone());
+        // Add the job to the history map
+        history.insert(jobId, HistoryEntry {
+            fileName,
+            timeCompleted: -1,
+        });
 
-        async_job
+        job_id
     }
 
-    pub async fn get_job(&self, peerId: &str) -> Option<AsyncConsumer> {
+    pub async fn get_job(&self, jobId: &str) -> Option<AsyncConsumer> {
         // Get a read lock on the jobs map
         let jobs = self.jobs.read().await;
 
         // Get the job
-        jobs.get(peerId).cloned()
+        jobs.get(jobId).cloned()
     }
     
     pub async fn get_job_history(&self) -> Vec<Job> {
         // Get a read lock on the job history
         let history = self.history.read().await;
 
-        history.clone()
+        history.values().cloned().collect()
     }
 
-    pub async fn remove_job_from_history(&self, peerId: &str) {
+    pub async fn remove_job_from_history(&self, jobId: &str) {
         // Get a write lock on the job history
         let mut history = self.history.write().await;
 
-        // Remove the job from the history
-        history.retain(|job| job.peerId != peerId);
+        history.remove(jobId);
     }
 
     pub async fn clear_job_history(&self) {
