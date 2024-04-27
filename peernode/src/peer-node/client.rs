6cf68b3d9214c493fabea65c907de0c06235ceeb
Jacky Xie <jackyxie2520@outlook.com>
2024-04-27 01:05:00 -0400

    Run market at startup

--- peernode/src/peer-node/client.rs
+++ peernode/src/peer-node/client.rs
@@ -20,6 +20,9 @@ async fn main() {
     let cli = cli();
     // Load the configuration
     let mut config = store::Configurations::new().await;
+    
+    // Run market client if it was previously configured
+    let _ = config.get_market_client().await;
 
     // check if there are any arguments passed to the program
     // if there are, process them and then exit
