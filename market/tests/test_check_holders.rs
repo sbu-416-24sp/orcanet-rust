eaf1695424e55e74e7c35b7e94782b2c2133d395
Jason Chong <jason.chong.1@stonybrook.edu>
2024-04-29 18:48:45 -0400

    test(peer-check_holders): add test for a basic check_holders test
    Closes: sbu-416-24sp/orcanet-rust#18

--- market/tests/test_check_holders.rs
+++ market/tests/test_check_holders.rs
@@ -2,7 +2,7 @@ use orcanet_market::{
     bridge::spawn, Config, FileInfoHash, FileResponse, ReqResSuccessfulResponse,
     SuccessfulResponse, SupplierInfo,
 };
-use proto::market::{FileInfo, User};
+use proto::market::{FileInfo, HoldersResponse, User};
 
 #[tokio::test]
 async fn test_register_file_and_get_self_holder() {
@@ -39,4 +39,33 @@ async fn test_register_file_and_get_self_holder() {
             }
         ))
     )
+}
+
+#[tokio::test]
+async fn test_register_file_and_check_holders_basic() {
+    let config = Config::builder().set_peer_tcp_port(3391).build();
+    let peer = spawn(config).unwrap();
+    let user = User {
+        id: "abc".to_string(),
+        name: "helloworld".to_string(),
+        ip: "127.0.0.1".to_string(),
+        port: 6666,
+        price: 32,
+    };
+    let file_info = FileInfo {
+        file_hash: "123abc".to_string(),
+        chunk_hashes: vec!["hi".to_string()],
+        file_size: 3212321,
+        file_name: "fooobar.mp4".to_owned(),
+    };
+    let expected_holders = HoldersResponse {
+        file_info: Some(file_info.clone()),
+        holders: vec![user.clone()],
+    };
+    let file_info_hash = FileInfoHash::new(file_info.hash_to_string());
+    let _ = peer
+        .register_file(user, file_info_hash.clone(), file_info)
+        .await;
+    let res = peer.check_holders(file_info_hash).await;
+    assert_eq!(res, Ok(SuccessfulResponse::CheckHolders(expected_holders)))
 }
